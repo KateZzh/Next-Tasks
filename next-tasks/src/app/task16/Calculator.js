924e6c30185ea977fffdf3b02290739c42ceacd7
KateZzh <ekaterin.pro2@gmail.com>
2024-01-18 21:34:06 +0300

    some changes to task 16

--- next-tasks/src/app/task16/Calculator.js
+++ next-tasks/src/app/task16/Calculator.js
@@ -1,9 +1,29 @@
+export default function Calculator(setVal, val) {
+  function getInputValue(e) {
+    setVal({ ...val, [e.target.name]: +e.target.value });
+  }
 
-export default function Calculator() {
+  function doAddition() {
+    setVal({ ...val, result: val.num1 + val.num2 });
+  }
 
-  return (
-    <>
-    
-    </>
-  )
+  function doSubtraction() {
+    setVal({ ...val, result: val.num1 - val.num2 });
+  }
+
+  function doMultiplication() {
+    setVal({ ...val, result: val.num1 * val.num2 });
+  }
+
+  function doDivision() {
+    val.num2 === 0 && setVal('Infinity');
+
+    setVal({ ...val, result: val.num1 / val.num2 });
+  }
+
+  function doClear() {
+    setVal({ ...val, num1: '', num2: '', result: '' });
+  }
+
+  return { getInputValue, doAddition, doSubtraction, doMultiplication, doDivision, doClear };
 }
--- next-tasks/src/app/task16/page.js
+++ next-tasks/src/app/task16/page.js
@@ -2,35 +2,11 @@
 
 import Link from 'next/link';
 import { useState } from 'react';
+import Calculator from './Calculator';
 
 export default function Task16() {
   const [val, setVal] = useState({ num1: '', num2: '', result: '' });
-
-  function getInputValue(e) {
-    setVal({ ...val, [e.target.name]: +e.target.value });
-  }
-
-  function doAddition() {
-    setVal({ ...val, result: val.num1 + val.num2 });
-  }
-
-  function doSubtraction() {
-    setVal({ ...val, result: val.num1 - val.num2 });
-  }
-
-  function doMultiplication() {
-    setVal({ ...val, result: val.num1 * val.num2 });
-  }
-
-  function doDivision() {
-    val.num2 === 0 && setVal('Infinity');
-    
-    setVal({ ...val, result: val.num1 / val.num2 });
-  }
-
-  function doClear() {
-    setVal({ ...val, num1: '', num2: '', result: '' });
-  }
+  const { getInputValue, doAddition, doSubtraction, doMultiplication, doDivision, doClear } = Calculator(setVal, val);
 
   return (
     <>
